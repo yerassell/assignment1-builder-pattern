759913ac182b82f83e542c00166754d647f226c6
yerassell <250577@astanait.edu.kz>
2026-09-11 21:14:25 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
 public class Main{
     public static void main(String[] args){
         Director director = new Director();
-        OfflineCourseBuilder offlineCourseBuilder = new OfflineCourseBuilder();
-        OnlineCourseBuilder onlineCourseBuilder = new OnlineCourseBuilder();
-
-        Course amcs = director.createAMCS(offlineCourseBuilder);
+        
+        Course amcs = director.createAMCS(new OfflineCourseBuilder());
         System.out.println(amcs);
 
-        Course sdp = director.createSDP(offlineCourseBuilder);
+        Course sdp = director.createSDP(new OfflineCourseBuilder());
         System.out.println(sdp);
 
-        Course web = director.createWEB(onlineCourseBuilder);
+        Course web = director.createWEB(new OnlineCourseBuilder());
         System.out.println(web);
 
-        Course os = director.createOS(onlineCourseBuilder);
+        Course os = director.createOS(new OnlineCourseBuilder());
         System.out.println(os);
     }
 }
