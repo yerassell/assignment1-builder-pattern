f176918b9808c3ec207173d577b6f975ccc27dcc
yerassell <250577@astanait.edu.kz>
2026-09-11 19:21:53 +0500

    Update OfflineCourseBuilder.java

--- OfflineCourseBuilder.java
+++ OfflineCourseBuilder.java
@@ -1,26 +1,17 @@
 public class OfflineCourseBuilder implements CourseBuilder{
-    private Course course;
-    private String roomNumber;
-    private boolean computersAvailable;
-    public OfflineCourseBuilder() {
-        course = new Course();
-    }
+    private final Course course = new Course();
 
-    public CourseBuilder name(String name) {
-        course.setName(name);
-        return this;
-    }
+    public CourseBuilder name(String name) {course.setName(name);return this;}
     public CourseBuilder instructor(String instructor) {course.setInstructor(instructor);return this;}
     public CourseBuilder courseType(String courseType) {course.setCourseType(courseType);return this;}
     public CourseBuilder credits(int credits) {course.setCredits(credits);return this;}
     public CourseBuilder schedule(String schedule) {course.setSchedule(schedule);return this;}
+    
     public OfflineCourseBuilder roomNumber(String roomNumber) {
-        this.roomNumber = roomNumber;
         course.setRoomNumber(roomNumber);
         return this;
     }
     public OfflineCourseBuilder computersAvailable(boolean computersAvailable) {
-        this.computersAvailable = computersAvailable;
         course.setComputersAvailable(computersAvailable);
         return this;
     }
